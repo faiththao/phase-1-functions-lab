caae19967c819affc0e48b42f8e22acb0ef5f5ee
Faith <thao.faith2@gmail.com>
2021-07-20 10:57:20 -0600

    commit tree 4bbf9076deadbcc798a342dea513f7e4da95a89d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let hqLocation = 42;
+function distanceFromHqInBlocks(start) {
+    return Math.abs(start - hqLocation);
+}
+
+function distanceFromHqInFeet(start) {
+    return Math.abs((start - hqLocation)*264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end)*264);
+}
+
+function calculatesFarePrice(someValue1, someValue2) {
+    let distance = (Math.abs(someValue1 - someValue2)*264);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400)*0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
